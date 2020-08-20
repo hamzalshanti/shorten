0836474499312bef9620cd5ae78c53e69b61a1b4
Hamza Alshanti <hamza2000pal@gmail.com>
2020-08-21 00:29:18 +0300

    updated1

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.use(express.json());
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log('server running'));
 
